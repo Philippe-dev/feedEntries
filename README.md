662724eb827c92db02fd33f21ccb6e9fc4ae00b3
“philippe <philippe@dissitou.org>
2022-12-07 09:11:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# feedEntries
+
+Integrate feed entries in your templates
+
+Forked from Pep's plugin: [feedEntries 1.0.2](https://plugins.dotaddict.org/dc2/details/feedEntries).
---
+++ _define.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @brief feedEntries, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Pep
+ *
+ * @copyright GPL-2.0 [https://www.gnu.org/licenses/gpl-2.0.html]
+ */
+if (!defined('DC_RC_PATH')) {
+    return;
+}
+
+$this->registerModule(
+    'feedEntries',
+    'Integrate feed entries in your templates',
+    'Pep',
+    '1.1'
+);
