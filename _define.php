105bf14c4ba6c5f4d6c8868b3d6c429bee2f5118
“philippe <philippe@dissitou.org>
2023-06-15 08:42:25 +0200

    Update _define.php

--- _define.php
+++ _define.php
@@ -9,22 +9,18 @@
  *
  * @copyright GPL-2.0 [https://www.gnu.org/licenses/gpl-2.0.html]
  */
-if (!defined('DC_RC_PATH')) {
-    return;
-}
-
 $this->registerModule(
     'feedEntries',
     'Integrate feed entries in your templates',
     'Pep',
-    '2.2',
+    '2.3',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
             dcAuth::PERMISSION_USAGE,
             dcAuth::PERMISSION_CONTENT_ADMIN,
         ]),
-        'type'     => 'plugin',
-        'support'    => 'https://github.com/Philippe-dev/feedEntries',
+        'type'    => 'plugin',
+        'support' => 'https://github.com/Philippe-dev/feedEntries',
     ]
 );
